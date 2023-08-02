0836b536fb8280b622004866f928f52bd226e6c4
HarshNinave <harshninave58@gmail.com>
2023-08-03 00:37:57 +0530

    Solved the Pattern questions

--- src/Patterns/Pattern8.java
+++ src/Patterns/Pattern8.java
@@ -1,17 +1,25 @@
 package Patterns;
 
 public class Pattern8 {
-
+        // * * * * *
+        //   * * * *
+        //     * * *
+        //       * *
+        //         *
     public static void pattern(int num){
-        for(int row=0;row<num;row++){
-            for(int col=1; col<=(num-row);col++){
+        for(int row=num;row>0;row--){
+
+            for (int spaces= 1; spaces<=(num-row) ; spaces++) {
+                System.out.print("  ");
+            }
+            // Note: With every iteration of spaces ,next for-loop will run
+            // i.e. Below (for-loop)
+            for(int col=1; col<=(row);col++){
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
-
-
     public static void main(String[] args) {
         pattern(5);
     }
