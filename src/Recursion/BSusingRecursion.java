d95923504fa2bf6e27a5af30acf53b04820f722b
HarshNinave <harshninave58@gmail.com>
2023-08-10 22:17:15 +0530

    Basic Recursion Questions

--- src/Recursion/BSusingRecursion.java
+++ src/Recursion/BSusingRecursion.java
@@ -3,11 +3,12 @@ package Recursion;
 // Doing binary search using recursion
 public class BSusingRecursion {
     public static void main(String[] args){
-        int[] arr = {2,4,5,12,21,32,41};
-        int target = 2;
+        int[] arr = {1,2,3,4,55,66,78};
+        int target = 55;
         System.out.println(search(arr,target,0,arr.length-1));
     }
     public static int search (int[] arr,int target,int start,int end){
+        
         if(start>end){
             return -1;
         }
